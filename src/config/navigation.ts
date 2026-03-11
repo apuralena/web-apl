7e87edf7bccba4b0c44f5ffeb0c8baf55b51432e
Lucas-Andres-GF <lucas.gallardo196304@alumnos.info.unlp.edu.ar>
2026-03-11 12:59:59 -0300

    style: agregar estilos globales, assets y utilidades

---
+++ src/config/navigation.ts
@@ -0,0 +1,14 @@
+export interface NavItem {
+  href: string;
+  text: string;
+}
+
+export const navItems: NavItem[] = [
+  { href: "/#inicio", text: "Inicio" },
+  { href: "/#eventos", text: "Eventos" },
+  { href: "/#propuestas", text: "Propuestas" },
+  { href: "/#experiencias", text: "Experiencias" },
+  { href: "/#nosotros", text: "Nosotros" },
+  { href: "/#contacto", text: "Contacto" },
+  { href: "/#presupuesto", text: "Presupuesto" },
+];
