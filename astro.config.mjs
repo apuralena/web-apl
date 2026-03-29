4e2986bc181b87e7a0e6f395f8fd9edbaf9d7477
Lucas-Andres-GF <lucas.gallardo196304@alumnos.info.unlp.edu.ar>
2026-03-29 00:09:10 -0300

    fix: improve favicon configuration for all browsers

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   site: "https://apuraleña.com.ar",
+  favicon: "/favicon.ico",
   integrations: [
     sitemap({
       changefreq: "weekly",
