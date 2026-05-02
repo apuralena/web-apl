75b7373f090549d1c9d9b661441ea5b9c4dac857
Lucas-Andres-GF <lucas.gallardo196304@alumnos.info.unlp.edu.ar>
2026-05-02 15:15:27 -0300

    fix: ajusta formato de importaciones y comentarios en archivos de navegación y lugares

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -1,15 +1,15 @@
 export interface NavItem {
-  href: string;
-  text: string;
+  href: string
+  text: string
 }
 
 export const navItems: NavItem[] = [
-  { href: "/#inicio", text: "Inicio" },
-  { href: "/#eventos", text: "Eventos" },
-  { href: "/#propuestas", text: "Propuestas" },
-  { href: "/#espacios", text: "Espacios" },
-  { href: "/#galeria", text: "Galería" },
-  { href: "/#nosotros", text: "Nosotros" },
-  { href: "/presupuesto", text: "Presupuesto" },
-  { href: "/#contacto", text: "Contacto" },
-];
+  { href: '/#inicio', text: 'Inicio' },
+  { href: '/#eventos', text: 'Eventos' },
+  { href: '/#propuestas', text: 'Propuestas' },
+  // { href: "/#espacios", text: "Espacios" },
+  { href: '/#galeria', text: 'Galería' },
+  { href: '/#nosotros', text: 'Nosotros' },
+  { href: '/presupuesto', text: 'Presupuesto' },
+  { href: '/#contacto', text: 'Contacto' },
+]
